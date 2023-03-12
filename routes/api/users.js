469a4153ffe98fd1e7525fed269220f6b3088f24
Natalia970970 <nataliasoldatenko970@gmail.com>
2023-03-12 17:13:30 +0200

    done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const ctrl = require('../../controllers/users')
-const {validateBody, authenticate} = require('../../midllewares');
+const {validateBody, authenticate, upload} = require('../../midllewares');
 const { authSchema } =  require('../../models/user');
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.post("/register", validateBody(authSchema), ctrl.register);
 router.post("/login", validateBody(authSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 module.exports = router;
